f6b80bd77f1b354643b135725d397b88bcaef330
Raj Bedi <35932733+BlueEmbers@users.noreply.github.com>
2018-11-15 12:33:31 -0500

    Add files via upload

---
+++ CSE218 L-04 Stacks and Queues/src/p3_Queues/QueueX.java
@@ -0,0 +1,61 @@
+package p3_Queues;
+
+public class QueueX 
+{
+	private int maxSize;
+	private long[] queArray;
+	private int front;
+	private int rear;
+	private int nElems;
+	
+	public QueueX(int maxSize)
+	{
+		this.maxSize = maxSize;
+		queArray = new long[this.maxSize];
+		front = 0;
+		rear = -1;
+		nElems = 0;
+	}
+	
+	public long remove()
+	{
+		long temp = queArray[front++];
+		if(front ==maxSize)
+		{
+			front =0;
+		}
+		nElems--;
+		return temp;
+	}
+	
+	public long peekFront()
+	{
+		return queArray[front];
+	}
+	
+	public boolean isEmpty()
+	{
+		return nElems == 0;
+	}
+	
+	public boolean isFull()
+	{
+		return nElems == maxSize;
+	}
+	
+	public int size()
+	{
+		return nElems;
+	}
+	
+	public void insert(long value)
+	{
+		if(rear == maxSize - 1)
+		{
+			rear = -1;
+		}
+		queArray[++rear] = value;
+		nElems++;
+	}
+
+}
